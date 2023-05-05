6931d0a95fbb8d7ef248723dfaf4606fa222d9c7
sakitam-fdd <smilefdd@gmail.com>
2023-05-05 22:12:46 +0800

    ✨ feat(eslint-config-vue): add eslint config for vue

---
+++ packages/eslint-config-vue3/README.md
@@ -0,0 +1,19 @@
+# @aurorafe/eslint-config-vue
+
+Vue3的eslint配置
+
+## 使用
+
+### 安装
+
+```bash
+pnpm add -D eslint @aurorafe/eslint-config-vue
+```
+
+### 配置 eslint (.eslintrc | .eslintrc.js | .eslintrc.json)
+
+```json
+{
+  "extends": "@aurorafe/eslint-config-vue"
+}
+```
---
+++ packages/eslint-config-vue3/index.js
@@ -0,0 +1,47 @@
+const { defineConfig } = require('eslint-define-config');
+
+module.exports = defineConfig({
+  root: true,
+  extends: ['plugin:vue/vue3-essential', '@vue/eslint-config-airbnb', '@aurorafe/eslint-config-ts'],
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue']
+      },
+      rules: {
+        'no-undef': 'off',
+        'vue/html-self-closing': 'off'
+      }
+    },
+    {
+      files: ['*.html'],
+      rules: {
+        'vue/comment-directive': 'off'
+      }
+    }
+  ],
+  rules: {
+    'vue/multi-word-component-names': [
+      'off',
+      {
+        ignores: ['index']
+      }
+    ],
+    'vue/html-self-closing': 'off',
+    'vue/no-v-model-argument': 'off',
+    'vue/first-attribute-linebreak': [
+      'error',
+      {
+        singleline: 'ignore',
+        multiline: 'below'
+      }
+    ],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/component-definition-name-casing': ['error', 'PascalCase'],
+    'vue/no-v-html': 'off'
+  }
+});
