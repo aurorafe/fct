1f7143301c40171418a973fb5e24b4eeb5e55359
sakitam-fdd <smilefdd@gmail.com>
2023-05-07 21:48:46 +0800

    ✨ feat(stylelint-config-vue): add stylelint config for vue

---
+++ packages/stylelint-config-vue/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    '@aurorafe/stylelint-config-base',
+    'stylelint-config-recommended-vue' // 必须要在最后一项, 因为其定义了customSyntax
+  ],
+  overrides: [
+    {
+      files: '**/*.scss',
+      customSyntax: 'postcss-scss',
+      extends: ['stylelint-config-standard-scss', 'stylelint-config-recommended-vue/scss']
+    }
+  ]
+};
